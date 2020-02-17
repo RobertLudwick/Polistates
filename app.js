ffd098c999bcf28a78f5ac68d7cfc2eb061a374b
Robert Ludwick <rludwick@drew.edu>
2020-02-17 14:08:14 -0500

    added code to start the webpage on localhols:3000

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Sean Larson")
+console.log("Sean Larson");
+
+var express = require('express');
+var app = express();
+var serve = require('http').Server('app');
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('client',express.static(__dirname + '/client'));
+
+app.listen(3000);
+
