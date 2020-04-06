aba9907c0eb52bc31e277956dc1c30b3a43f4bee
idavis97 <idavis@drew.edu>
2020-04-06 11:45:27 -0400

    Added more game mechanics

--- public/Scripts/turn.js
+++ public/Scripts/turn.js
@@ -1,22 +1,77 @@
+function selectGovt1() {
+    //Checks if first turn
+    var element = document.getElementById('currentTurn').innerHTML;
+    if (element < 1) {
+    //Initializing country values based on government type
+    var currency = document.getElementById('currency').innerHTML;
+    currency = parseInt(currency) + 2500;
+    document.getElementById('currency').innerHTML = currency;
+
+    var resources = document.getElementById('resources').innerHTML;
+    resources = parseInt(resources) + 3000;
+    document.getElementById('resources').innerHTML = resources;
+    }
+}
+
+function selectGovt2() {
+    //Checks if first turn
+    var element = document.getElementById('currentTurn').innerHTML;
+    if (element < 1) {
+    //Initializing country values based on government type
+     var currency = document.getElementById('currency').innerHTML;
+    currency = parseInt(currency) + 5000;
+    document.getElementById('currency').innerHTML = currency;
+
+    var resources = document.getElementById('resources').innerHTML;
+    resources = parseInt(resources) + 1000;
+    document.getElementById('resources').innerHTML = resources;
+    }
+}
+
 function nextTurn() {
+    //Increase turn number
     var element = document.getElementById('currentTurn').innerHTML;
     element++;
     document.getElementById('currentTurn').innerHTML = element;
 
+    //Increase country values
     var population = document.getElementById('population').innerHTML;
-    population = parseInt(population) + 50;
+    population = parseInt(population) + 1000;
     document.getElementById('population').innerHTML = population;
 
     var currency = document.getElementById('currency').innerHTML;
-    currency = parseInt(currency) + 2000;
+    currency = parseInt(currency) + 2500;
     document.getElementById('currency').innerHTML = currency;
 
     var resources = document.getElementById('resources').innerHTML;
-    resources = parseInt(resources) + 250;
+    resources = parseInt(resources) + 1500;
     document.getElementById('resources').innerHTML = resources;
     
-    var happiness = document.getElementById('happiness').innerHTML;
-    happiness = parseInt(happiness)+5;
-    document.getElementById('happiness').innerHTML = happiness;
-    
+}
+
+function scenario1(){
+    var worldfair = confirm("Would you like to contribute to the world fair?");
+
+    if (worldfair){
+        //Pull the values 
+        var population = document.getElementById('population').innerHTML;
+        population = parseInt(population) - 100;
+        document.getElementById('population').innerHTML = population;
+
+        var currency = document.getElementById('currency').innerHTML;
+        currency = parseInt(currency) - 1000;
+        document.getElementById('currency').innerHTML = currency;
+
+        var resources = document.getElementById('resources').innerHTML;
+        resources = parseInt(resources) - 500;
+        document.getElementById('resources').innerHTML = resources;
+        
+        //Increase amount of wf(worldfair) contributions
+        var wf = document.getElementById('worldfair').innerHTML;
+        wf++;
+        document.getElementById('worldfair').innerHTML = wf;
+    }
+    else{
+        alert("You didn't contribute to the world fair... shame!");
+    }
 }
